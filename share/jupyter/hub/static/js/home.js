112ab5eee2508d45a12b912cbd210e679716e5e2
Clement Nero <clement.nero@fr.pwc.com>
2017-04-03 14:28:58 +0200

    Move out javascript code from home.html

--- share/jupyter/hub/static/js/home.js
+++ share/jupyter/hub/static/js/home.js
@@ -1,13 +1,14 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-require(["jquery", "jhapi"], function ($, JHAPI) {
+require(["jquery", "jhapi", "flipclock"], function ($, JHAPI) {
     "use strict";
     
     var base_url = window.jhdata.base_url;
     var user = window.jhdata.user;
     var api = new JHAPI(base_url);
-    
+    var clock;
+
     $("#stop").click(function () {
         api.stop_server(user, {
             success: function () {
@@ -15,5 +16,25 @@ require(["jquery", "jhapi"], function ($, JHAPI) {
             }
         });
     });
-    
+
+    api.get_user(user,{
+        success: function (user) {
+            var diff = user.total_time;
+            var autoStart = false;
+            if ($("#stop").is(':visible')) {
+                var currentDate = new Date(user.utc_now);
+                var pastDate  = new Date(user.last_spwaner_start);
+                diff += (currentDate.getTime() / 1000 - pastDate.getTime() / 1000)
+                autoStart = true
+            }
+            clock = $('.clock').FlipClock(diff, {
+                clockFace: 'DailyCounter',
+                autoStart: autoStart
+            });
+            $("#stop").on("spawner:stop", function(){
+                clock.stop()
+            })
+        }
+    })
+
 });
